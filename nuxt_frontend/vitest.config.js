ddfde9c4470148791f2b46068d4907abea562c71
Plotnikov Aleksandr <shukik85@ya.ru>
2025-10-14 23:24:44 +0300

    Create vitest.config.js

---
+++ nuxt_frontend/vitest.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/**',
+        'tests/**',
+        '*.config.js',
+        '.nuxt/**',
+        'dist/**'
+      ]
+    },
+    include: ['tests/**/*.test.js'],
+    setupFiles: [],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './')
+    }
+  }
+})
