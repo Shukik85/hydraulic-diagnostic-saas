1dc4bd34f07d4575fbb2e9877e469eeb960ab328
Plotnikov Aleksandr <shukik85@ya.ru>
2025-10-14 23:15:04 +0300

    Create vitest.config.js

---
+++ frontend/vitest.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'tests/',
+        '*.config.js'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
