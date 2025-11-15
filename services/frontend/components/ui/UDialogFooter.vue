7592894bb8a720e4e2ed326a3e4be20a43e1eed2
Plotnikov Aleksandr <shukik85@ya.ru>
2025-11-16 01:42:49 +0300

    feat(ui): migrate Dialog family to PascalCase+U (5 components)
    - UDialog.vue: Teleport + animated modal, metallic styling, close button
    - UDialogHeader.vue / UDialogTitle.vue / UDialogDescription.vue / UDialogFooter.vue
    
    Accessibility, props/className, focus management.
    
    Progress: 36/46 components (78%)

---
+++ services/frontend/components/ui/UDialogFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="cn('flex flex-col-reverse gap-2 sm:flex-row sm:justify-end', props.className)" v-bind="$attrs">
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+import { cn } from './utils'
+interface Props { className?: string }
+const props = withDefaults(defineProps<Props>(), {})
+</script>
---
+++ services/frontend/components/ui/UDialogHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="cn('flex flex-col gap-2 text-center sm:text-left', props.className)" v-bind="$attrs">
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+import { cn } from './utils'
+interface Props { className?: string }
+const props = withDefaults(defineProps<Props>(), {})
+</script>
