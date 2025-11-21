333a8161b40b1691197b07247dcd23dd32514152
Plotnikov Aleksandr <shukik85@ya.ru>
2025-11-21 03:13:33 +0300

    refactor(gnn_service): move stub files to _legacy and create clean structure

---
+++ services/gnn_service/_legacy/dataset_dynamic_stub.py
@@ -0,0 +1,9 @@
+# services/gnn_service/dataset_dynamic.py
+"""
+DynamicTemporalGraphDataset:
+PyTorch Dataset для любого набора компонентов/сенсоров/графа.
+[...CONTENTS OF dataset_dynamic.py (as in [58])...]
+"""
+# LEGACY STUB - moved to _legacy on 2025-11-21
+# This file contained only a comment placeholder
+# See new implementation in services/gnn_service/src/data/dataset.py
---
+++ services/gnn_service/_legacy/model_dynamic_gnn_stub.py
@@ -0,0 +1,8 @@
+# services/gnn_service/model_dynamic_gnn.py
+"""
+Universal Dynamic GNN Architecture для гидравлических систем.
+[...CONTENTS OF model_dynamic_gnn.py (as in [57])...]
+"""
+# LEGACY STUB - moved to _legacy on 2025-11-21
+# This file contained only a comment placeholder
+# See new implementation in services/gnn_service/src/models/gnn_model.py
