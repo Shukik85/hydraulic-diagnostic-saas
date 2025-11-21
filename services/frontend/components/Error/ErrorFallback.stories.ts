3c26f5c4f6f78356aafac6230e5113ceeb517eef
Plotnikov Aleksandr <shukik85@ya.ru>
2025-11-21 22:28:39 +0300

    fix(storybook): использовать явную типизацию Meta в ErrorFallback

--- services/frontend/components/Error/ErrorFallback.stories.ts
+++ services/frontend/components/Error/ErrorFallback.stories.ts
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import ErrorFallback from './ErrorFallback.vue'
 
-const meta = {
+const meta: Meta<typeof ErrorFallback> = {
   title: 'Error/ErrorFallback',
   component: ErrorFallback,
   tags: ['autodocs'],
-} satisfies Meta<typeof ErrorFallback>
+}
 
 export default meta
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof ErrorFallback>
 
 export const Default: Story = {
   args: {
